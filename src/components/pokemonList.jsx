341b20611d728493d4730ca40eef7d230b191568
hebaselim <heba.darweesh1@gmail.com>
2020-12-23 16:36:53 +0200

    Adding pokemon favicon

--- src/components/pokemonList.jsx
+++ src/components/pokemonList.jsx
@@ -1,18 +1,33 @@
 import React, { Component } from "react";
 class PokemonList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedPokemonURL: "https://pokeapi.co/api/v2/pokemon/1/",
+      selectedPokemonName: "",
+     
+    };
+  }
+  toggleClass = (pokemon) => {
+    this.setState({ selectedPokemonURL: pokemon.url, selectedPokemonName:pokemon.name });
+  };
   render() {
     return (
-      <div className="list-group">
-        {this.props.pokemonList["results"].map((pokemon) => (
-          <ol
-            href={pokemon.url}
-            className="list-group-item list-group-item-action"
-            key={pokemon.url}
-          >
-            {pokemon.url.split("/")[6]} {pokemon.name}
-          </ol>
-        ))}
-      </div>
+      <ul style={{ width: "50%", marginTop: "10px" }} className="list-group">
+      {this.props.pokemonList["results"].map((pokemon) => (
+        <li
+          className={
+            this.state.selectedPokemonURL === pokemon.url
+              ? "list-group-item list-group-item-action active"
+              : "list-group-item list-group-item-action"
+          }
+          key={pokemon.url}
+          onClick={() => this.toggleClass(pokemon)}
+        >
+          {pokemon.url.split("/")[6]} {pokemon.name}
+        </li>
+      ))}
+    </ul>
     );
   }
 }
