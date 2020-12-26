c8c6a047382f8ffc3482f2361e79777ede2bb67c
hebaselim <heba.darweesh1@gmail.com>
2020-12-26 04:12:50 +0200

    Adding Api constants File

---
+++ src/apiConstants.js
@@ -0,0 +1,3 @@
+export const LIST_API = "https://pokeapi.co/api/v2/pokemon/?limit=1200";
+export const DETAILS_API = "https://pokeapi.co/api/v2/pokemon/";
+export const EVOLUTION_API ="https://pokeapi.co/api/v2/evolution-chain/";
