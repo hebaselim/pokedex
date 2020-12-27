677186334403adb95f49de59fbe7853e0b740933
hebaselim <heba.darweesh1@gmail.com>
2020-12-27 21:16:44 +0200

    move onHover to utils

--- src/apiConstants.js
+++ src/apiConstants.js
@@ -1,3 +1,5 @@
-export const LIST_API = "https://pokeapi.co/api/v2/pokemon/?limit=1200";
+export const LIST_API = "https://pokeapi.co/api/v2/pokemon/?limit=898";
 export const DETAILS_API = "https://pokeapi.co/api/v2/pokemon/";
-export const EVOLUTION_API ="https://pokeapi.co/api/v2/evolution-chain/";
+export const SPECIES_API = "https://pokeapi.co/api/v2/pokemon-species/";
+export const IMG_URL =
+  "https://assets.pokemon.com/assets/cms2/img/pokedex/full/";
--- src/utils/helpers.jsx
+++ src/utils/helpers.jsx
@@ -1,25 +1,42 @@
+import { DETAILS_API } from "../apiConstants";
+
 export function getIdFromUrl(url) {
-    return url.split("/")[6];
-  }
+  return url.split("/")[6];
+}
 
 export function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
-export function findKey(object,key) {
-    let objects = [];
-    const rec = (o) => {
-      for (let k in o) {
-        if (typeof o[k] === "object") {
-          if (k === key) {
-            objects.push(o[k]);
-          }
-          rec(o[k]);
+export function findKey(object, key) {
+  let objects = [];
+  const rec = (o) => {
+    for (let k in o) {
+      if (typeof o[k] === "object") {
+        if (k === key) {
+          objects.push(o[k]);
         }
+        rec(o[k]);
       }
-    };
-    rec(object);
-    return objects;
-  }
+    }
+  };
+  rec(object);
+  return objects.reverse();
+}
 
- 
+export function padStart(id) {
+  return id.padStart(3, "0");
+}
+
+export function onHover(id, url = DETAILS_API) {
+  if (!localStorage.getItem(id)) {
+    fetch(url + id)
+      .then((response) => response.json())
+      .then((response) => {
+        localStorage.setItem(id, JSON.stringify({ details: response }));
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      });
+  }
+}
